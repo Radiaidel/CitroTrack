e2f21bcbf84a3c4d10e790d1ac688d173a3543da
radia idelkadi <idelkadiradia@gmail.com>
2024-11-17 21:50:58 +0100

    CIT-27 Initial setup of Spring project with dependencies

---
+++ src/main/java/com/aidar/citrotrack/Citrotrack2Application.java
@@ -0,0 +1,13 @@
+package com.aidar.citrotrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Citrotrack2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Citrotrack2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/aidar/citrotrack/Citrotrack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidar.citrotrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Citrotrack2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
